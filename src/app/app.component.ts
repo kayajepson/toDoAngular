d1e8fe5560d018f8a0b078d4e641796c5a5669d5
brian hensley and kaya jepson <brian@>
2019-06-10 11:08:07 -0700

    start todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('finish homework'),
+    new Task('wash dog'),
+    new Task('clean house')
+  ];
 }
