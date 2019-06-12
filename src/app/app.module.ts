5e72eb4053499284b93e202393c4b0361dc3a835
heather zurek and kaya jepson <heather@>
2019-06-12 15:14:01 -0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
